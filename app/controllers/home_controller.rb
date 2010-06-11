a6702432778043e959c9f7d18ddb5a6d883638a9
Tobias Strebitzer <tobias.strebitzer@holzweg2.com>
2010-06-11 18:23:55 +0200

    Users and survey integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,22 @@
+require 'yaml'
+
 class HomeController < ApplicationController
   def index
+    # Get param user
+    if params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = User.find(1)
+    end
+    
+    # Load Survey
+    @config = YAML::load(File.open("#{RAILS_ROOT}/app/fixtures/survey.yml"))
+    debugger
+
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @user }
+    end
   end
 
 end
